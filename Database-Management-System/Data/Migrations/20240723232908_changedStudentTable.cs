1b6271316b4c5767832214768e1554439dbd322d
SahibSingh-SahibSingh <200564714@student.georgianc.on.ca>
2024-07-23 19:29:31 -0400

    changes in student table

---
+++ Database-Management-System/Data/Migrations/20240723232908_changedStudentTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Database_Management_System.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class changedStudentTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "StudentNumber",
+                table: "Students",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StudentNumber",
+                table: "Students");
+        }
+    }
+}
--- Database-Management-System/Models/Student.cs
+++ Database-Management-System/Models/Student.cs
@@ -3,6 +3,7 @@
     public class Student
     {
         public int StudentId {  get; set; }
+        public int StudentNumber {  get; set; }
         public string StudentName { get; set; }
         public string ProgramName {  get; set; }
         public DateOnly RegistrationDate { get; set; }
