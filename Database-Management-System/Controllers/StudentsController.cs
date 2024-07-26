cd3d101daec28303cddeab1f184b66235e4c0eda
SahibSingh-SahibSingh <200564714@student.georgianc.on.ca>
2024-07-26 15:43:08 -0400

    try to deploy

--- Database-Management-System/Controllers/StudentsController.cs
+++ Database-Management-System/Controllers/StudentsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Database_Management_System.Data;
 using Database_Management_System.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Database_Management_System.Controllers
 {
@@ -44,6 +45,7 @@ namespace Database_Management_System.Controllers
         }
 
         // GET: Students/Create
+        [Authorize]
         public IActionResult Create()
         {
             return View();
@@ -52,6 +54,7 @@ namespace Database_Management_System.Controllers
         // POST: Students/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("StudentId,StudentNumber,StudentName,ProgramName,RegistrationDate,StudentEmail")] Student student)
@@ -66,6 +69,7 @@ namespace Database_Management_System.Controllers
         }
 
         // GET: Students/Edit/5
+        [Authorize]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -84,6 +88,7 @@ namespace Database_Management_System.Controllers
         // POST: Students/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("StudentId,StudentNumber,StudentName,ProgramName,RegistrationDate,StudentEmail")] Student student)
@@ -117,6 +122,7 @@ namespace Database_Management_System.Controllers
         }
 
         // GET: Students/Delete/5
+        [Authorize]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -135,6 +141,7 @@ namespace Database_Management_System.Controllers
         }
 
         // POST: Students/Delete/5
+        [Authorize]
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
